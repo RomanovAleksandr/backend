64fef7722a42593417c96f27b20ca7b02da3d4b8
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2020-03-19 20:07:19 +0300

    remove_duplicates

---
+++ lab2/remove_duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                string input = args[0];
+                input = new string(input.Distinct().ToArray());
+                Console.WriteLine(input);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect number of arguments!");
+                Console.WriteLine("Usage remove_duplicates.exe <input string>");
+            }
+        }
+    }
+}
