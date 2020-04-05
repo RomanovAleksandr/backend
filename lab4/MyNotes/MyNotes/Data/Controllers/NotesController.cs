d4ab8a36b182a7994337354141097359dbc8920c
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2020-04-05 12:26:13 +0300

    MyNotes

---
+++ lab4/MyNotes/MyNotes/Data/Controllers/NotesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyNotes.Data.Interfaces;
+
+namespace MyNotes.Data.Controllers
+{
+    public class NotesController : Controller
+    {
+        private readonly INotesRepositories _notesRepositories;
+
+        public NotesController(INotesRepositories notesRepositories)
+        {
+            _notesRepositories = notesRepositories;
+        }
+    }
+}
---
+++ lab4/MyNotes/MyNotes/Data/Repositories/NotesRepository.cs
@@ -0,0 +1,37 @@
+﻿using MyNotes.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyNotes.Data.Models;
+using System.IO;
+
+namespace MyNotes.Data.Repositories
+{
+    public class NotesRepository : INotesRepositories
+    {
+        string notesPath = "Data/Notes.txt";
+        public IEnumerable<Note> GetAll()
+        {
+            StreamReader notesReader = new StreamReader(notesPath);
+            List<Note> notesList = new List<Note>();
+
+            string line;
+            while ((line = notesReader.ReadLine()) != null)
+            {
+                notesList.Add(new Note { message = line });
+            }
+
+            notesReader.Close();
+
+            return notesList;
+        }
+
+        public void Add(Note note)
+        {
+            StreamWriter notesWriter = new StreamWriter(notesPath, true);
+            notesWriter.WriteLine(note.message);
+            notesWriter.Close();
+        }
+    }
+}
