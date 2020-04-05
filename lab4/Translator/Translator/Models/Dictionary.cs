3d951b852e59c9da2b9240d14f61b98ce566218a
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2020-04-05 09:56:32 +0300

    Translator

---
+++ lab4/Translator/Translator/Models/Dictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Translator.Models
+{
+    public class Dictionary
+    {
+        StreamReader dictionary = new StreamReader("Models/Dictionary.txt");
+
+        public string findTranslation(string word)
+        {
+            word = word.ToLower();
+
+            string line;
+            while ((line = dictionary.ReadLine()) != null)
+            {
+                string[] words = line.Split(' ');
+                if (words[0] == word)
+                {
+                    return words[1];
+                }
+                else if (words[1] == word)
+                {
+                    return words[0];
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ lab4/Translator/Translator/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Translator.Models;
+
+namespace Translator
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        private static void HandleWord(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                var word = context.Request.Query["word"];
+
+                Dictionary dictionary = new Dictionary();
+                string translatedWord = dictionary.findTranslation(word);
+
+                if (translatedWord != null)
+                {
+                    context.Response.ContentType = "text/plain;charset=utf-8";
+                    await context.Response.WriteAsync($"{word} = {translatedWord}");
+                }
+                else
+                {
+                    context.Response.StatusCode = 404;
+                    await context.Response.WriteAsync("Not Found");
+                }
+                
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.MapWhen(context => context.Request.Query.ContainsKey("word"),
+                               HandleWord);
+
+            app.Run(async context =>
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Bad Request");
+            });
+        }
+    }
+}
